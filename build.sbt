95a37d222e43c345ea8ca8e25d63e4b53f54ebad
Adam Thompson <adamthompson993@gmail.com>
2021-11-09 22:36:54 -0600

    Actually got a line parser working

--- build.sbt
+++ build.sbt
@@ -5,5 +5,7 @@ lazy val json5Parser = (project in file("."))
   .settings(
     name := "json5-scala",
     libraryDependencies += "org.scalactic" %% "scalactic" % "3.2.10",
-    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % "test"
+    libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.10" % "test",
+    libraryDependencies += "org.typelevel" %% "cats-core" % "2.6.1",
+    libraryDependencies += "org.typelevel" %% "kittens" % "2.3.2"
   )
--- src/main/scala/parser/JsonParser.scala
+++ src/main/scala/parser/JsonParser.scala
@@ -1,14 +1,38 @@
 package parser
 
+import cats.data.StateT
+import cats.implicits._
+
 class JsonParser {
 
-  sealed trait Parser[A] {
-    type parse = String => Option[(String, A)]
-  }
+  type Parser[A] = StateT[Option, String, A]
 
-  def jsonNull: Parser[JsonValue] = ???
+  //Parser that just checks if there is a character at all
+  lazy val item: Parser[Char] = for {
+    input <- StateT.get[Option, String]
+    _ <-
+      if (input.nonEmpty)
+        StateT.modify[Option, String](_.tail)
+      else
+        ().raiseError[Parser, Nothing]
+  } yield input.head
 
-  def charP: Char => Parser[Char] = ???
+  //Create a generic parser that can take a boolean check as a value
+  def sat(p: Char => Boolean): Parser[Char] = for {
+    c <- item
+    _ <-
+      if (p(c)) c.pure[Parser]
+      else ().raiseError[Parser, Nothing]
+  } yield c
 
-  case class ParserError(line: Int, char: Int, error: String)
+  def char(c: Char): Parser[Char] = sat(_ == c)
+
+  /**   Sequence is some weirdness that basically inverts the types. So we provide a List[Chars] => List[Parsers]
+    *   sequence List[Parser[Char]] => Parser[List[Char]]
+    */
+  def string(str: String): Parser[String] =
+    str.map(char).toList.sequence.map(_.mkString)
+
+  val digit: Parser[Char] = sat(_.isDigit)
+
 }
